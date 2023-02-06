643fa711df285039d86ec72d27c2987f3d0977a3
MoresMask <byasov.fd@gmail.com>
2023-02-06 10:43:19 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       // home: MyHomePage(),
-      home: MainPage(),
+      home: MyHomePage(),
     );
   }
 }
